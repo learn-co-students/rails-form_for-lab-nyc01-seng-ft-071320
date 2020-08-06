35d1edb8b8c2c57b3ec979de2b5e2ef7860479c6
pairofAces <karanchauhan16@gmail.com>
2020-08-06 00:24:17 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+student1 = Student.create([{ first_name: 'karan', last_name: 'chauhan'}])
+student2 = Student.create([{ first_name: 'ran', last_name: 'cha'}])
+student3 = Student.create([{ first_name: 'ka', last_name: 'han'}])
+
+school_class1 = SchoolClass.create([{title: 'computer science', room_number: '11'}])
+school_class2 = SchoolClass.create([{title: 'science', room_number: '5'}])
+school_class3 = SchoolClass.create([{title: 'math', room_number: '1'}])
